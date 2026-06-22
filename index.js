a0e3db6b6da89688ef9316699a56c37d209a5792
Gopal Yadav <yadavgopal49350@gmail.com>
2026-06-22 12:21:00 +0530

    Updated index.js with all route connections

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Import routes
+const studentRoutes = require("./routes/studentRoute");
+const courseRoutes = require("./routes/courseRoute");
+const attendanceRoutes = require("./routes/attendanceRoute");
+const marksRoutes = require("./routes/marksRoute");
+
+// Connect DB
 connectDB();
 
+// Default route
 app.get("/", (req, res) => {
   res.send("Server running");
 });
+
+// Register routes
+app.use("/api/students", studentRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/attendance", attendanceRoutes);
+app.use("/api/marks", marksRoutes);
 
 const PORT = process.env.PORT || 5000;
 
